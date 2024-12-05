0e13b5ac18a91fa87a6fa048b4fd01fd8d4894db
santiagoahumada13 <124843945+santiagoahumada13@users.noreply.github.com>
2024-12-04 21:13:18 -0600

    Delete build directory
    lo subi x error jaja

--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/santiago/dev/pico/proyecto-final/build/_deps/picotool/"
-};
--- build/src/CMakeFiles/proyectofinal.dir/opt/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/proyectofinal.dir/opt/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
- /opt/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
